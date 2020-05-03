675e416a1ded6c67c57cfdd706a4421277b3a9bd
AbhishekDoshi26 <adoshi26.ad@gmail.com>
2020-05-03 22:14:34 +0530

    Live Plot

---
+++ Matplotlib/live plot.py
@@ -0,0 +1,26 @@
+import matplotlib.animation as animation
+from matplotlib import style
+import matplotlib.pyplot as plt
+
+plt.style.use('fivethirtyeight')
+fig1 = plt.figure()
+axis1 = fig1.add_subplot(1, 1, 1)
+
+
+def animate(p):
+    plot_data = open(
+        'D:\python-programs\Matplotlib\Plot using csv\data.txt', 'r').read()
+    line_data = plot_data.split('\n')
+    x1 = []
+    y1 = []
+    for line in line_data:
+        if len(line) > 1:
+            x, y = line.split(',')
+            x1.append(x)
+            y1.append(y)
+        axis1.clear()
+        axis1.plot(x1, y1)
+
+
+anime_data = animation.FuncAnimation(fig1, animate, interval=500)
+plt.show()
