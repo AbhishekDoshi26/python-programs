f7e2085a362b1f4d258886b0c70069b0bbd21a42
AbhishekDoshi26 <adoshi26.ad@gmail.com>
2020-05-03 21:50:05 +0530

    Ploting data using csv and txt file

--- Plot using csv/program.py
+++ Plot using csv/program.py
@@ -1,1 +1,26 @@
 import matplotlib.pyplot as plt
+import csv
+
+x = []
+y1 = []
+y2 = []
+
+with open('D:\python-programs\Plot using csv\data.txt', 'r')as datafile:
+    plots = csv.reader(datafile)
+    for col in plots:
+        x.append(col[0])
+        y1.append(col[1])
+
+with open('D:\python-programs\Plot using csv\data.csv', 'r')as datafile:
+    plots = csv.reader(datafile)
+    for col in plots:
+        y2.append(col[1])
+
+
+plt.plot(x, y1, label='Text File Data')
+plt.plot(x, y2, label='CSV File Data')
+plt.xlabel('X Axis')
+plt.ylabel('Y Axis')
+plt.title('Plot using txt and csv file')
+plt.legend()
+plt.show()
