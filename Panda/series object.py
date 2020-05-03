dcde6e44f2fa0089467ce61747082e73141a324e
AbhishekDoshi26 <adoshi26.ad@gmail.com>
2020-05-03 22:29:38 +0530

    series object

---
+++ Panda/series object.py
@@ -0,0 +1,5 @@
+import pandas as pd
+subjects = ['TOC', 'AJ', 'WT', 'SE', 'Dot Net']
+pd.Series(subjects)
+print('\nData without series object: ', subjects)
+print('\nData with series object: \n', pd.Series(subjects))
