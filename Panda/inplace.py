8eb6743a8b36cd96344745f771df79bd40431c60
AbhishekDoshi26 <adoshi26.ad@gmail.com>
2020-05-06 22:59:28 +0530

    Update inplace.py

--- Panda/inplace.py
+++ Panda/inplace.py
@@ -1,2 +1,6 @@
 import pandas as pd
 y = pd.read_csv('data.csv', squeeze=True, usecols=['Y-Axis'])
+y.sort_values(inplace=True, ascending=False)
+# inplace replaces the original list with the sorted list.
+# It is like: y=y.sort(ascending=False)
+print(y)
