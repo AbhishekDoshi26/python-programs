284dc2fedbe7345e9e0f45904237db9e65eeaf84
AbhishekDoshi26 <adoshi26.ad@gmail.com>
2020-05-13 18:09:49 +0530

    between

---
+++ Panda/between.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+employees = pd.read_csv('Datasets\employees.csv', parse_dates=[
+                        "Start Date", "Last Login Time"])
+employees['Senior Management'] = employees['Senior Management'].astype(bool)
+employees['Gender'] = employees['Gender'].astype("category")
+
+print(employees[employees['Bonus %'].between(15, 20)])
