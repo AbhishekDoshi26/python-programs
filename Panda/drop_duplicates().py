cad531dbd2a0faf170fed4a40b9a65218e645bcc
AbhishekDoshi26 <adoshi26.ad@gmail.com>
2020-05-13 18:35:11 +0530

    drop_duplicates()

---
+++ Panda/drop_duplicates().py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+employees = pd.read_csv('Datasets\employees.csv', parse_dates=[
+                        "Start Date", "Last Login Time"])
+employees['Senior Management'] = employees['Senior Management'].astype(bool)
+employees['Gender'] = employees['Gender'].astype("category")
+
+employees.sort_values('First Name', inplace=True)
+
+employees.drop_duplicates(subset=['First Name', 'Team'], inplace=True)
+print(employees)
